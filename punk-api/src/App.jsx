910b175ffb5a97d9af0a8647724ed994c13fdbd5
Nicholas <blueretrograde@gmail.com>
2022-10-21 14:47:29 +0100

    created repo and initialised react app & installed sass dependencies

---
+++ punk-api/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header>
+        <h1>PUNK API</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
